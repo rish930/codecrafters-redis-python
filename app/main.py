ea6ac43356d46b649a2e9f52c2fc2a9b9611895d
rish930 <rishabh930@gmail.com>
2024-08-01 20:40:37 +0530

    add support for concurrent clients

--- app/main.py
+++ app/main.py
@@ -1,17 +1,23 @@
 # Uncomment this to pass the first stage
 import socket
+import threading
 
+def respond_to_ping(server_socket: socket.socket):
+    client, addr = server_socket.accept()
+    while data:=client.recv(1024):
+        client.send(b"+PONG\r\n")
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
+    concurrent_clients = 2
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
-    with socket.create_server(("localhost", 6379), reuse_port=True) as server_socket:
-        client, addr = server_socket.accept() # wait for client
-        while data:=client.recv(1024):
-            client.send(b"+PONG\r\n")
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    threads = []
+    for i in range(concurrent_clients):
+        t = threading.Thread(target=respond_to_ping, kwargs={"server_socket":server_socket})
+        print(f"starting thread {i}")
+        t.start()
+    
 
 if __name__ == "__main__":
     main()
